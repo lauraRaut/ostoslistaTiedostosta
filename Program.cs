64451de3c2a974bf838a035070c8174dad5fdfe5
Laura.Raut <laura.rautjoki@gmail.com>
2022-06-08 12:12:04 +0300

    added list printing method to console

--- Program.cs
+++ Program.cs
@@ -16,16 +16,39 @@ namespace harjoitustehtavaTiedostot
 
                 Console.WriteLine("Kirjoita tuote: ");
                 string tuote = Console.ReadLine();
+                
+
+
 
             if (File.Exists(polku))
             {
                 File.AppendAllText(polku, tuote + Environment.NewLine);
+                
+                try
+                {
+                    String[] lines;
+                    lines = File.ReadAllLines(polku);
+
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        Console.WriteLine("hei");
+                        Console.WriteLine(lines[i]);
+                    }
+
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Tiedostoa ei voida lukea");
+                    Console.WriteLine(e.Message);
+                }
             }
             else
             {
                 Console.WriteLine("Tiedostoa ei löydy.");
             }
-                }
+            Console.ReadKey();
+        }
+        
             }
         }
     
