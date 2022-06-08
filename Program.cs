3d00ea5c4abcc76718e2a1f09b54ab1c9ce2af7b
Laura.Raut <laura.rautjoki@gmail.com>
2022-06-08 10:54:41 +0300

    Start new project

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harjoitustehtavaTiedostot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+       
+            String polku = @"C:\Users\laura\source\repos\harjoitustehtavaTiedostot\harjoitustehtavaTiedostot\harjoitustehtavaTiedostot.txt";
+
+                Console.WriteLine("Kirjoita tuote: ");
+                string tuote = Console.ReadLine();
+
+            if (File.Exists(polku))
+            {
+                File.AppendAllText(polku, tuote + Environment.NewLine);
+            }
+            else
+            {
+                Console.WriteLine("Tiedostoa ei löydy.");
+            }
+                }
+            }
+        }
+    
+
